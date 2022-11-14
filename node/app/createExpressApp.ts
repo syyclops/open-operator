ba96a470f1dfd9ed7eb02284c1889d58fb5439b5
demattosanthony <demattosanthony@gmail.com>
2022-11-14 15:39:35 -0500

    ontology apis to upload ontology and building graphs

--- node/app/createExpressApp.ts
+++ node/app/createExpressApp.ts
@@ -3,8 +3,28 @@ import dotenv from 'dotenv'
 dotenv.config()
 import type { ErrorRequestHandler } from 'express'
 import router from './routes/createRouter'
+import swaggerJsDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import glob from 'glob'
 
 const app: Express = express()
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Hello World',
+      version: '1.0.0',
+    },
+  },
+  apis: glob
+    .sync('**/*.js', { cwd: `${__dirname}/routes/api/` })
+    .map((filename) => 'dist/app/routes/api/' + filename), // files containing annotations as above
+}
+
+const swaggerDocs = swaggerJsDoc(options)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {}
 
--- node/app/routes/api/v1/building/getBuilding.ts
+++ node/app/routes/api/v1/building/getBuilding.ts
@@ -2,8 +2,24 @@ import { Router } from 'express'
 import neo4j_driver from '../../../../config/neo4j_driver'
 import { Building } from '../../../../types/building'
 
-// Get Building
-// @param: building uri
+/**
+ * @swagger
+ * /api/v1/building/{uri}:
+ *   get:
+ *     description: Get Building by uri
+ *     parameters:
+ *     - in: path
+ *       name: uri
+ *       description: building uri
+ *       type: string
+ *       required: true
+ *       minimum: 1
+ *     responses:
+ *       200:
+ *         description: OK
+ *     tags:
+ *       - building
+ */
 module.exports = Router({ mergeParams: true }).get(
   '/v1/building/:uri',
   async (req, res) => {
