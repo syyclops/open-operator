e62d2cc27901f7a387c6f2695de31bade549d5f0
demattosanthony <demattosanthony@gmail.com>
2023-11-22 15:54:21 -0500

    gpt classifier for bacnet points buts its way to expensive

--- gpt_classifier.py
+++ gpt_classifier.py
@@ -1,236 +1,26 @@
 from rdflib import Graph, Namespace
 from dotenv import load_dotenv
 from openai import OpenAI
+from tqdm import tqdm
+import tiktoken
 load_dotenv()
 
 client = OpenAI()
 
-prompt = """BRICK SCHEMA EQUIPMENT TYPES:
 
-Equipment
-Elevator
-Gas Distribution
-Relay
-Steam Distribution
-Water Distribution
-Weather Station
-Camera
-Surveillance Camera
-Furniture
-Stage Riser
-Solar Thermal Collector
-PVT Panel
-Motor
-Variable Frequency Drive
-VFD
-Fan VFD
-Heat Wheel VFD
-Pump VFD
-Shading Equipment
-Automatic Tint Window
-Blind
-Water Heater
-Collection Basin Water Heater
-Boiler
-Electric Boiler
-Natural Gas Boiler
-Condensing Natural Gas Boiler
-Noncondensing Natural Gas Boiler
-Lighting Equipment
-Interface
-Touchpanel
-Switch
-Dimmer
-Lighting
-Luminaire
-Luminaire Driver
-Safety Equipment
-First Aid Kit
-AED
-Automated External Defibrillator
-Emergency Wash Station
-Drench Hose
-Eye Wash Station
-Safety Shower
-Security Equipment
-Intrusion Detection Equipment
-Access Control Equipment
-Access Reader
-Intercom Equipment
-Emergency Phone
-Video Intercom
-Video Surveillance Equipment
-NVR
-Network Video Recorder
-PV Panel
-Fire Safety Equipment
-Fire Alarm
-Fire Alarm Control Panel
-Fire Control Panel
-Heat Detector
-Smoke Detector
-Manual Fire Alarm Activation Equipment
-Fire Alarm Manual Call Point
-Fire Alarm Pull Station
-Valve
-Gas Valve
-Natural Gas Seismic Shutoff Valve
-HVAC Valve
-Chilled Water Valve
-Condenser Water Valve
-Makeup Water Valve
-Water Valve
-Thermostatic Mixing Valve
-Hot Water Valve
-Domestic Hot Water Valve
-Preheat Hot Water Valve
-Electrical Equipment
-Breaker Panel
-Bus Riser
-Disconnect Switch
-Inverter
-Motor Control Center
-PlugStrip
-Switchgear
-Transformer
-Energy Storage
-Battery
-Meter
-Thermal Power Meter
-Electrical Meter
-Building Electrical Meter
-Gas Meter
-Building Gas Meter
-Water Meter
-Building Water Meter
-Chilled Water Meter
-Building Chilled Water Meter
-Hot Water Meter
-Building Hot Water Meter
-Building Meter
-HVAC Equipment
-Compressor
-Condenser
-Cooling Tower
-Cooling Valve
-Dry Cooler
-Economizer
-Fume Hood
-Humidifier
-Space Heater
-Steam Valve
-Thermostat
-Air Handler Unit
-Air Handling Unit
-CRAH
-Cold Deck
-Computer Room Air Conditioning
-Computer Room Air Handler
-HX
-Hot Deck
-Isolation Valve
-Condenser Water Isolation Valve
-Pump
-Water Pump
-Chilled Water Pump
-Condenser Water Pump
-Hot Water Pump
-Bypass Valve
-Condenser Water Bypass Valve
-Differential Pressure Bypass Valve
-CRAC
-Standby CRAC
-Air Plenum
-Return Air Plenum
-Discharge Air Plenum
-Supply Air Plenum
-Underfloor Air Plenum
-Chiller
-Absorption Chiller
-Centrifugal Chiller
-Heating Valve
-Reheat Valve
-Return Heating Valve
-Heat Exchanger
-Condenser Heat Exchanger
-Evaporative Heat Exchanger
-Heat Wheel
-Coil
-Cooling Coil
-Chilled Water Coil
-Direct Expansion Cooling Coil
-Heating Coil
-Direct Expansion Heating Coil
-Hot Water Coil
-Filter
-Final Filter
-Intake Air Filter
-Mixed Air Filter
-Pre Filter
-Return Air Filter
-Damper
-Economizer Damper
-Exhaust Damper
-Mixed Damper
-Outside Damper
-Relief Damper
-Return Damper
-AHU
-PAU
-DOAS
-Dedicated Outdoor Air System Unit
-Dual Duct Air Handling Unit
-MAU
-Makeup Air Unit
-RTU
-Rooftop Unit
-DDAHU
-Fan
-Booster Fan
-Ceiling Fan
-Cooling Tower Fan
-Exhaust Fan
-Outside Fan
-Relief Fan
-Return Fan
-Standby Fan
-Supply Fan
-Transfer Fan
-Discharge Fan
-Terminal Unit
-Constant Air Volume Box
-Fan Coil Unit
-Induction Unit
-CAV
-FCU
-VAV
-Chilled Beam
-Active Chilled Beam
-Passive Chilled Beam
-Variable Air Volume Box
-Variable Air Volume Box With Reheat
-RVAV
-Air Diffuser
-Displacement Flow Air Diffuser
-Jet Nozzle Air Diffuser
-Laminar Flow Air Diffuser
-Radiator
-Electric Radiator
-Electric Baseboard Radiator
-Hot Water Radiator
-Hot Water Baseboard Radiator
-Steam Radiator
-Steam Baseboard Radiator
-Baseboard Radiator
-Radiant Panel
-Embedded Surface System Panel
-Radiant Ceiling Panel
-Thermally Activated Building System Panel
-ESS Panel
-RC Panel
-TABS Panel
+### Give all the bacnet device a brick class
 
-You are a system that is given a bacnet device name and you output whats its correct brick schema classification class is. ONLY output the brick class. If the brick class is unknown output N/A.
+device_classifier_prompt = """BRICK SCHEMA EQUIPMENT TYPES:\n\n"""
+
+# Read in all brick schema equipment types from brick_equipment_list.txt.
+# This is used to create the prompt for the GPT-3 API.
+with open("./brick_equipment_list.txt", "r") as f:
+    brick_equipment_list = f.read()
+    
+# Add the brick equipment list to the prompt
+device_classifier_prompt = device_classifier_prompt + brick_equipment_list
+
+device_classifier_prompt += """\nYou are a system that is given a bacnet device name and you output whats its correct brick schema classification class is. ONLY output the brick class. If the brick class is unknown output N/A.
     
 Device name: AHU-1
 
@@ -261,36 +51,121 @@ WHERE {
 }
 """
 
+print("Classifying devices...")
 # Run the query for devices
-for row in g.query(query_for_devices):
-    device_uri = row[0]
-    device_name = row[1]
+# for row in tqdm(g.query(query_for_devices)):
+#     device_uri = row[0]
+#     device_name = row[1]
 
-    input = prompt + device_name + "\n" + "Brick Class: "
+#     input = device_classifier_prompt + device_name + "\n" + "Brick Class: "
 
-    response = client.completions.create(
-      model="gpt-3.5-turbo-instruct",
-      prompt=input,
-      temperature=0
-    )
+#     response = client.completions.create(
+#       model="gpt-3.5-turbo-instruct",
+#       prompt=input,
+#       temperature=0
+#     )
 
-    # Get the response
-    response = response.choices[0].text
+#     # Get the response
+#     response = response.choices[0].text
 
-    # Remove any whitespace at the beginning or end
-    response = response.strip()
+#     # Remove any whitespace at the beginning or end
+#     response = response.strip()
 
-    if response != "N/A":
-        print(device_name + " is a " + response)
+#     if response != "N/A":
+#         # Replace all whitespace with _
+#         brick_class = BRICK[response.replace(" ", "_")]
 
-        # Replace all whitespace with _
-        brick_class = BRICK[response.replace(" ", "_")]
-        print(brick_class)
+#         # Add the brick class to the graph
+#         g.add((device_uri, A, brick_class))
 
-        # Add the brick class to the graph
-        g.add((device_uri, A, brick_class))
 
+### Give all the bacnet points a brick class
+
+# Load all the brick classes for points from brick_point_list.txt
+with open("./brick_point_list.txt", "r") as f:
+    brick_point_list = f.read()
+
+point_classifier_sys_message = """BRICK SCHEMA POINTS:\n\n"""
+point_classifier_sys_message += brick_point_list + "\n"
+point_classifier_sys_message += """You are a system that is given bacnet object information and you output whats its correct brick schema classification class is. ONLY output the brick class. If the brick class is unknown output N/A."""
+
+# Define a SPARQL query for bacnet_Point
+query_for_points = """
+PREFIX bacnet: <http://data.ashrae.org/bacnet/2016#>
+
+SELECT ?point ?device_name ?point_name ?present_value ?unit ?object_type ?description
+WHERE {
+  ?point a bacnet:bacnet_Point ;
+         bacnet:device_name ?device_name ;
+         bacnet:object_name ?point_name ;
+         bacnet:present_value ?present_value ;
+         bacnet:object_units ?unit ;
+         bacnet:raw_object_type ?object_type ;
+         bacnet:object_description ?description .
+}
+"""
+
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
+  """Returns the number of tokens used by a list of messages."""
+  try:
+      encoding = tiktoken.encoding_for_model(model)
+  except KeyError:
+      encoding = tiktoken.get_encoding("cl100k_base")
+  if model == "gpt-3.5-turbo-0613":  # note: future models may deviate from this
+      num_tokens = 0
+      for message in messages:
+          num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+          for key, value in message.items():
+              num_tokens += len(encoding.encode(value))
+              if key == "name":  # if there's a name, the role is omitted
+                  num_tokens += -1  # role is always required and always 1 token
+      num_tokens += 2  # every reply is primed with <im_start>assistant
+      return num_tokens
+  else:
+      raise NotImplementedError(f"""num_tokens_from_messages() is not presently implemented for model {model}.
+  See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+
+print("Classifying points...")
+# Run the query for points
+for row in tqdm(g.query(query_for_points)):
+    point, device_name, point_name, present_value, unit, object_type, description = row
+    
+    input = "Bacnet Device Name: " + device_name.value + "\n" + "Bacnet Point Name: " + point_name.value + "\n" + "Bacnet Point Present Value: " + present_value.value + "\n" + "Bacnet Point Unit: " + unit.value + "\n" + "Bacnet Point Description: " + description.value + "\n"
+
+    messages = [
+        {
+            "role": "system",
+            "content": point_classifier_sys_message
+        },
+        {
+            "role": "user",
+            "content": input
+        }
+    ]
+
+    num_tokens = num_tokens_from_messages(messages)
+    print("Number of tokens:", num_tokens)
+
+    # res = client.chat.completions.create(messages=messages, model="gpt-4-1106-preview")
+
+    # response = res.choices[0].message.content
+    
+
+    # # Remove any whitespace at the beginning or end
+    # response = response.strip()
+
+    # if response != "N/A":
+    #     # Replace all whitespace with _
+    #     brick_class = BRICK[response.replace(" ", "_")]
+
+    #     # Add the brick class to the graph
+    #     g.add((point, A, brick_class))
+
+# Run inference on the graph to add inferred types
+g.expand("rdfs")
 # Save the graph
 g.serialize("data/dunbar/brick.ttl", format="turtle")
 
-    
+# If the user exits the program, save the graph
+import atexit
+atexit.register(g.serialize, "data/dunbar/brick.ttl", format="turtle")
