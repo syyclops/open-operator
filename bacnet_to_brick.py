b42b8111c2a18c642708e31a656696278a068edb
demattosanthony <demattosanthony@gmail.com>
2023-11-27 16:39:19 -0500

    better finding optimal k, and fixed bugs in classification. Think im getting rate limited though

--- bacnet_to_brick.py
+++ bacnet_to_brick.py
@@ -12,6 +12,7 @@ from sklearn.cluster import KMeans
 import argparse
 import numpy as np
 from langchain.embeddings.openai import OpenAIEmbeddings
+import time
 load_dotenv()
 
 # Create the OpenAI client
@@ -33,47 +34,93 @@ BACNET = Namespace("http://data.ashrae.org/bacnet/2016#")
 A = RDF['type']
 
 
-def find_optimal_k(X, kmin=2, kmax=200, sample_size=10000, batch_size=1000):
+"""
+This function finds the optimal number of clusters for KMeans clustering.
+It uses the silhouette score to determine the optimal number of clusters.
+
+Parameters:
+    X (np.array): The data to be clustered.
+    kmin (int, optional): The minimum number of clusters to test. Defaults to 2.
+    kmax (int, optional): The maximum number of clusters to test. Defaults to 200.
+    batch_size (int, optional): The batch size to use for MiniBatchKMeans. Defaults to 1000.
+    patience (int, optional): The number of iterations to wait for the silhouette score to improve before stopping. Defaults to 5.
+    step_size (int, optional): The step size to use when iterating through the number of clusters. Defaults to 10.
+
+Returns:
+    int: The optimal number of clusters.
+"""
+def find_optimal_k(X, kmin=2, kmax=200, batch_size=1000, patience=5, step_size=10):
     print("Finding optimal number of clusters...")
     sil = [] # list of silhouette scores
+    counter = 0
+    best_score = -1
+    k_values = []
 
-    for k in tqdm(range(kmin, kmax+1)):
-        kmeans = MiniBatchKMeans(n_clusters=k, n_init=10).fit(X) # adjust batch_size as needed
+    for k in tqdm(range(kmin, kmax+1, step_size)):
+        kmeans = MiniBatchKMeans(n_clusters=k, n_init=10, random_state=0, batch_size=batch_size).fit(X) # adjust batch_size as needed
         labels = kmeans.labels_
-        sil.append(silhouette_score(X, labels, metric='euclidean')) # adjust sample_size as needed
+        score = silhouette_score(X, labels, metric='euclidean')
+        sil.append(score)
+        k_values.append(k)
 
-        # Early stopping
-        if k > 2 and sil[k-kmin] < sil[k-kmin-1]:
-            break
+        # Early stopping with patience
+        if score > best_score:
+            best_score = score
+            counter = 0  # reset counter when score improves
+        else:
+            counter += 1  # increment counter when score does not improve
+            if counter >= patience:
+                break
 
-    optimal_clusters = np.argmax(sil) + kmin  # +kmin because the range starts from kmin
+    optimal_clusters = k_values[np.argmax(sil)]  # get the optimal number of clusters
 
     return optimal_clusters
 
-def kmeans_clustering(X, k):
+"""
+This function performs KMeans clustering on the given data.
+
+Parameters:
+    X (np.array): The data to be clustered.
+    k (int): The number of clusters.
+
+Returns:
+    tuple: A tuple containing the cluster assignments and the labels.
+"""
+def kmeans_clustering(X: np.array, k: int):
     print("Clustering...")
-    kmeans = KMeans(n_clusters=k, random_state=0)
+    kmeans = KMeans(n_clusters=k, random_state=0, n_init=10)
     cluster_assignments = kmeans.fit_predict(X)
     labels = kmeans.labels_
 
     return cluster_assignments, labels
 
 
-def load_bacnet_devices(g):
-    print("Loading bacnet devices into vector store...")
+"""
+This function vectorizes the bacnet graph. It creates documents for each device and point in the graph,
+and loads these documents into the vector store. It uses SPARQL queries to extract the necessary information
+from the graph.
+
+Parameters:
+    g (Graph): The bacnet graph to be vectorized.
+
+Returns:
+    tuple: A tuple containing the device documents and the point documents.
+"""
+def vectorize_bacnet_graph(g):
+    print("Vectorizing bacnet graph...")
     # Create the documents to load into the vector store
-    documents = []
+    device_documents = []
 
     # Define a SPARQL query for bacnet_Device
     query_for_devices = """
-    PREFIX bacnet: <http://data.ashrae.org/bacnet/2016#>
+        PREFIX bacnet: <http://data.ashrae.org/bacnet/2016#>
 
-    SELECT ?device ?device_name
-        WHERE {
-        ?device a bacnet:bacnet_Device ;
-                bacnet:device_name ?device_name .
-        }
-    """
+        SELECT ?device ?device_name
+            WHERE {
+            ?device a bacnet:bacnet_Device ;
+                    bacnet:device_name ?device_name .
+            }
+        """
 
     # Run the query for devices
     for row in g.query(query_for_devices):
@@ -82,11 +129,38 @@ def load_bacnet_devices(g):
 
         # Create the document
         content = device_name.value
-        documents.append(Document(page_content=content, metadata={"type": "bacnet_device", "uri": device_uri}))
-    
-    langchain_chroma.add_documents(documents)
+        device_documents.append(Document(page_content=content, metadata={"type": "bacnet_device", "uri": device_uri}))
 
-    return documents
+    # Load the documents into the vector store
+    langchain_chroma.add_documents(device_documents)
+    
+    points_documents = []
+
+    # Define query for bacnet_Point
+    query_for_points = """
+        PREFIX bacnet: <http://data.ashrae.org/bacnet/2016#>
+
+        SELECT ?point ?device_name ?point_name ?present_value ?unit
+        WHERE {
+        ?point a bacnet:bacnet_Point ;
+                bacnet:device_name ?device_name ;
+                bacnet:object_name ?point_name ;
+                bacnet:present_value ?present_value ;
+                bacnet:object_units ?unit .
+        }
+        """
+    
+    # Run the query for points
+    for row in g.query(query_for_points):
+        point, device_name, point_name, present_value, unit = row
+        
+        content = point_name.value + " " + present_value.value + " " + unit.value
+        points_documents.append(Document(page_content=content, metadata={"type": "bacnet_point", "uri": point}))
+
+    # Load the documents into the vector store
+    langchain_chroma.add_documents(points_documents)
+
+    return device_documents, points_documents
 
 
 def load_brick_equipment_types():
@@ -135,58 +209,76 @@ def load_brick_point_types():
     return documents
 
 
-def classify_bacnet_device_cluster(cluster, g):
-    print("Classifying a cluster of devices...")
-    device_names = []
-    for document in cluster:
-        device_names.append(document.page_content)
+def classify_bacnet_clusters(clusters: dict, g: Graph, filter_type: str):
+    try:
+        print(f"Classifying {type} clusters...")
+        for cluster in tqdm(clusters):
+            cluster = clusters[cluster] # Get the cluster from the dictionary
+            
+            device_names = []
+            for document in cluster:
+                device_names.append(document.page_content)
 
-    # Similarity search for the device name
-    results = langchain_chroma.similarity_search("\n".join(device_names), k=10, filter={"type": "brick_equipment"})
+            # Similarity search for the device name
+            results = langchain_chroma.similarity_search("\n".join(device_names), k=10, filter={"type": filter_type})
 
-    # Get the page content for the similar brick classes
-    similar_brick_classes = [result.page_content for result in results]
+            # Get the page content for the similar brick classes
+            similar_brick_classes = [result.page_content for result in results]
 
-    # Create the prompt
-    device_classifier_prompt = """You are a system that is given a cluster bacnet device names and you output whats its correct brick schema classification class is for the cluster. ONLY output the brick class. ONLY output one brick class. ONLY select a brick class from the provided brick equipment types. If the brick class is unknown output N/A.\n\n"""
-    device_classifier_prompt += """BRICK SCHEMA EQUIPMENT TYPES:\n\n"""
-    device_classifier_prompt += "\n".join(similar_brick_classes) + "\n"
+            # Create the prompt
+            device_classifier_prompt = """You are bacnet to brick schema classifier. You are given a cluster of bacnet device names or object data and you output the correct brick schema classification class. You are given the options of brick classes to choose from, select the one that makes the most sense for the bacnet cluster.
 
-    messages = [
-        {
-            "role": "system",
-            "content": device_classifier_prompt
-        },
-        # {
-        #     "role": "user",
-        #     "content": "AHU-1\nAHU-2\nAHU-5"
-        # },
-        # {
-        #     "role": "assistant",
-        #     "content": "AHU"
-        # },
-        {
-            "role": "user",
-            "content": "\n".join(device_names)
-        }
-    ]
+You ONLY output the brick class. 
+You ONLY output one brick class. 
+You ONLY select a brick class from the provided brick schema class options. 
+If the brick class is unknown output N/A."""
+            
+            input = "BRICK SCHEMA CLASS OPTIONS:\n"
+            input += "\n".join(similar_brick_classes) + "\n\n"
+            input += "BACNET Cluster:\n"
+            input += "\n".join(device_names)
 
-    # Send the prompt to the API
-    res = client.chat.completions.create(messages=messages, model="gpt-4-1106-preview")
+            print(device_classifier_prompt)
+            print()
+            print(input)
+            print()
 
-    # Get the response
-    response = res.choices[0].message.content
+            messages = [
+                {
+                    "role": "system",
+                    "content": device_classifier_prompt
+                },
+                {
+                    "role": "user",
+                    "content": input
+                }
+            ]
 
-    # Remove any whitespace at the beginning or end
-    response = response.strip()
+            # Send the prompt to the API
+            res = client.chat.completions.create(messages=messages, model="gpt-3.5-turbo-1106", temperature=0, max_tokens=100)
 
-    if response != "N/A":
-        # Replace all whitespace with _
-        brick_class = BRICK[response.replace(" ", "_")]
+            # Get the response
+            response = res.choices[0].message.content
 
-        # Add the brick class to the graph
-        for document in cluster:
-            g.add((document.metadata["uri"], A, brick_class))
+            print(response)
+            print()
+            print()
+
+            # Remove any whitespace at the beginning or end
+            response = response.strip()
+
+            if response != "N/A":
+                # Replace all whitespace with _
+                brick_class = BRICK[response.replace(" ", "_")]
+
+                # Add the brick class to the graph
+                for document in cluster:
+                    g.add((document.metadata["uri"], A, brick_class))
+
+            # wait some time so we don't get rate limited
+            # time.sleep(25)
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 def main():
@@ -208,7 +300,7 @@ def main():
     assert output, 'Output file must be specified'
 
     # Load the graph
-    g = Graph()
+    g = Graph(load_brick=True)
     g.bind('brick', BRICK)
     g.bind('rdf', RDF)
     g.load_file(path, format="turtle")
@@ -216,16 +308,18 @@ def main():
     # Load the brick equipment types into the vector store
     load_brick_equipment_types()
     load_brick_point_types()
-    # Load the bacnet devices into the vector store
-    documents = load_bacnet_devices(g)
+
+    # Vectorize the bacnet graph data
+    device_documents, points_documents = vectorize_bacnet_graph(g)
 
     # Get the embeddings of the bacnet devices
     device_embds = langchain_chroma.get(include=["embeddings", "metadatas"], where={"type": "bacnet_device"})
     device_embds = device_embds["embeddings"]
     device_embds = np.vstack(device_embds)
 
-    # Find the optimal number of clusters
-    optimal_clusters = find_optimal_k(device_embds)
+    # Find the optimal number of clusters for the bacnet devices
+    optimal_clusters = find_optimal_k(device_embds, kmin=2, kmax=40, step_size=1)
+    print("Optimal number of clusters of bacnet devices: " + str(optimal_clusters))
 
     # Cluster the bacnet devices
     cluster_assignments, labels = kmeans_clustering(device_embds, optimal_clusters)
@@ -236,177 +330,39 @@ def main():
         cluster = cluster_assignments[i]
         if cluster not in clusters:
             clusters[cluster] = []
-        clusters[cluster].append(documents[i]) 
+        clusters[cluster].append(device_documents[i]) 
 
-    # Classify each cluster
-    for cluster in clusters:
-        classify_bacnet_device_cluster(clusters[cluster], g=g)
+    # Classify the bacnet devices
+    classify_bacnet_clusters(clusters, g, filter_type="brick_equipment")
 
+    # Get the embeddings of the bacnet points
+    points_embds = langchain_chroma.get(include=["embeddings", "metadatas"], where={"type": "bacnet_point"})
+    points_embds = points_embds["embeddings"]
+    points_embds = np.vstack(points_embds)
+
+    # Find the optimal number of clusters for the bacnet points
+    optimal_clusters = find_optimal_k(points_embds, kmin=260, kmax=360, step_size=5)
+    print("Optimal number of clusters of bacnet points: " + str(optimal_clusters))
+
+    # Cluster the bacnet points
+    cluster_assignments, labels = kmeans_clustering(points_embds, optimal_clusters)
+
+    # Create a dictionary of clusters, with the key being the cluster number and the value being the list of documents and metadata
+    clusters = {}
+    for i in range(len(cluster_assignments)):
+        cluster = cluster_assignments[i]
+        if cluster not in clusters:
+            clusters[cluster] = []
+        clusters[cluster].append(points_documents[i])
+
+    # Classify the bacnet points
+    classify_bacnet_clusters(clusters, g, filter_type="brick_point")
     
     # Save the graph
     print(f"Before: {len(g)} triples")
     g.expand("rdfs")
     print(f"After: {len(g)} triples")
     g.serialize(output, format="turtle")
-        
     
-    # Print the clusters
-    # for cluster in clusters:
-    #     print("Cluster " + str(cluster) + ":")
-    #     for document in clusters[cluster]:
-    #         print(document.metadata["uri"])
-    #     print()
-    
-
 if __name__ == "__main__":
     main()
-
-
-# # # Define a SPARQL query for bacnet_Device
-# # query_for_devices = """
-# # PREFIX bacnet: <http://data.ashrae.org/bacnet/2016#>
-
-# # SELECT ?device ?device_name
-# # WHERE {
-# #   ?device a bacnet:bacnet_Device ;
-# #           bacnet:device_name ?device_name .
-# # }
-# # """
-
-# # print("Classifying devices...")
-# # # Run the query for devices
-# # for row in tqdm(g.query(query_for_devices)):
-# #     device_uri = row[0]
-# #     device_name = row[1]
-
-# #     # Similarity search for the device name
-# #     results = db.similarity_search(device_name.value, k=10)
-
-# #     # Get the page content for the similar brick classes
-# #     similar_brick_classes = [result.page_content for result in results]
-
-# #     # Create the prompt
-# #     device_classifier_prompt = """BRICK SCHEMA EQUIPMENT TYPES:\n\n"""
-# #     device_classifier_prompt += "\n".join(similar_brick_classes) + "\n"
-# #     device_classifier_prompt += """\nYou are a system that is given a bacnet device name and you output whats its correct brick schema classification class is. ONLY output the brick class. If the brick class is unknown output N/A.
-    
-# # Device name: AHU-1
-
-# # Brick Class: AHU
-
-# # Device name:"""
-
-# #     input = device_classifier_prompt + device_name + "\n" + "Brick Class: "
-
-# #     # Send the prompt to the API
-# #     response = client.completions.create(
-# #       model="gpt-3.5-turbo-instruct",
-# #       prompt=input,
-# #       temperature=0
-# #     )
-
-# #     # Get the response
-# #     response = response.choices[0].text
-
-# #     # Remove any whitespace at the beginning or end
-# #     response = response.strip()
-
-# #     if response != "N/A":
-# #         # Replace all whitespace with _
-# #         brick_class = BRICK[response.replace(" ", "_")]
-
-# #         # Add the brick class to the graph
-# #         g.add((device_uri, A, brick_class))
-
-
-# # # Load all the brick classes for points from brick_point_list.txt
-# with open("./brick_point_list.txt", "r") as f:
-#     brick_point_list = f.read()
-
-# # Create an array of all the brick classes for points by separating by commas
-# brick_point_list = brick_point_list.split(",")
-
-# print("Loading brick schema point types into vector store...")
-# # Load all the brick classes for points from brick_point_list.txt into the vector store
-# for line in tqdm(brick_point_list):
-#     content = line
-
-#     # Create a document
-#     document = Document(page_content=content, metadata={"type": "brick_point"})
-#     documents.append(document)
-
-# # Load the documents into the vector store
-# db = Chroma.from_documents(documents, OpenAIEmbeddings()) 
-
-
-# # Define a SPARQL query for bacnet_Point
-# query_for_points = """
-# PREFIX bacnet: <http://data.ashrae.org/bacnet/2016#>
-
-# SELECT ?point ?device_name ?point_name ?present_value ?unit ?object_type ?description
-# WHERE {
-#   ?point a bacnet:bacnet_Point ;
-#          bacnet:device_name ?device_name ;
-#          bacnet:object_name ?point_name ;
-#          bacnet:present_value ?present_value ;
-#          bacnet:object_units ?unit ;
-#          bacnet:raw_object_type ?object_type ;
-#          bacnet:object_description ?description .
-# }
-# """
-
-# print("Classifying points...")
-# try :
-#     # Run the query for points
-#     for row in tqdm(g.query(query_for_points)):
-#         point, device_name, point_name, present_value, unit, object_type, description = row
-        
-#         input = "Bacnet Device Name: " + device_name.value + "\n" + "Bacnet Point Name: " + point_name.value + "\n" + "Bacnet Point Present Value: " + present_value.value + "\n" + "Bacnet Point Unit: " + unit.value + "\n" + "Bacnet Point Description: " + description.value + "\n"
-
-#         # Similarity search for the point name
-#         results = db.similarity_search(point_name.value, k=10)
-
-#         # Get the page content for the similar brick classes
-#         similar_brick_classes = [result.page_content for result in results]
-
-#         point_classifier_sys_message = """BRICK SCHEMA POINTS:\n\n"""
-#         point_classifier_sys_message += "\n".join(similar_brick_classes) + "\n"
-#         point_classifier_sys_message += """\nYou are a system that is given bacnet object information and you output whats its correct brick schema classification class is. ONLY output the brick class. If the brick class is unknown output N/A."""
-
-
-#         messages = [
-#             {
-#                 "role": "system",
-#                 "content": point_classifier_sys_message
-#             },
-#             {
-#                 "role": "user",
-#                 "content": input
-#             }
-#         ]
-
-#         # Send the prompt to the API
-#         res = client.chat.completions.create(messages=messages, model="gpt-3.5-turbo")
-
-#         # Get the response
-#         response = res.choices[0].message.content
-
-#         # Remove any whitespace at the beginning or end
-#         response = response.strip()
-
-#         if response != "N/A":
-#             # Replace all whitespace with _
-#             brick_class = BRICK[response.replace(" ", "_")]
-
-#             # Add the brick class to the graph
-#             g.add((point, A, brick_class))  
-# except KeyboardInterrupt:
-#     print("Keyboard Interrupt, saving graph...")
-#     # Run inference on the graph to add inferred types
-#     g.expand("rdfs")
-#     g.serialize("data/dunbar/brick.ttl", format="turtle")
-    
-# # Run inference on the graph to add inferred types
-# g.expand("rdfs")
-# # Save the graph
-# g.serialize("data/dunbar/brick.ttl", format="turtle")
