8fe6c86051a3fddb6c106949e15f05149d1a4a98
demattosanthony <demattosanthony@gmail.com>
2024-02-16 09:16:19 -0500

    test_bas to test_bacnet

--- tests/test_bacnet.py
+++ tests/test_bacnet.py
@@ -1,10 +1,10 @@
-from openoperator.core import BAS
+from openoperator.core import BACnet 
 import unittest
 from unittest.mock import Mock, patch
 import json
 from rdflib import Namespace, RDF, URIRef, Literal
 
-class TestBAS(unittest.TestCase):
+class TestBACnet(unittest.TestCase):
   @patch('openoperator.services.knowledge_graph.KnowledgeGraph')
   def setUp(self, mock_knowledge_graph):
     mock_kg = mock_knowledge_graph.return_value
@@ -12,7 +12,7 @@ class TestBAS(unittest.TestCase):
     facility.knowledge_graph = mock_kg
     facility.uri = "https://openoperator.com/exampleCustomer/exampleFacility"
     embeddings = Mock()
-    self.bas = BAS(facility, embeddings)
+    self.bacnet = BACnet(facility, embeddings)
 
   def setup_session_mock(self):
     # Create the session mock
@@ -22,7 +22,7 @@ class TestBAS(unittest.TestCase):
     # Simulate exiting the context
     session_mock.__exit__ = Mock(return_value=None)
     # Configure the knowledge_graph to return this session mock
-    self.bas.knowledge_graph.create_session.return_value = session_mock
+    self.bacnet.knowledge_graph.create_session.return_value = session_mock
     return session_mock
   
   def test_devices(self):
@@ -45,7 +45,7 @@ class TestBAS(unittest.TestCase):
     ]
     session_mock.run.return_value = mock_query_result
 
-    devices = self.bas.devices()
+    devices = self.bacnet.devices()
 
     assert len(devices) == 2
     assert devices[0]['device_name'] == "test_device"
@@ -72,7 +72,7 @@ class TestBAS(unittest.TestCase):
     ]
     session_mock.run.return_value = mock_query_result
 
-    points = self.bas.points()
+    points = self.bacnet.points()
 
     assert len(points) == 2
     assert points[0]['point_name'] == "test_point"
@@ -99,7 +99,7 @@ class TestBAS(unittest.TestCase):
     ]
     session_mock.run.return_value = mock_query_result
 
-    points = self.bas.points(device_uri="https://openoperator.com/facility/device")
+    points = self.bacnet.points(device_uri="https://openoperator.com/facility/device")
 
     assert len(points) == 2
     assert points[0]['point_name'] == "test_point"
@@ -155,7 +155,7 @@ class TestBAS(unittest.TestCase):
   
   def test_convert_bacnet_data_to_rdf(self):
     bacnet_data = self.create_bacnet_json_data()
-    g = self.bas.convert_bacnet_data_to_rdf(bacnet_data)
+    g = self.bacnet.convert_bacnet_data_to_rdf(bacnet_data)
     
     BACNET = Namespace("http://data.ashrae.org/bacnet/#")
     device_uri = URIRef("https://openoperator.com/exampleCustomer/exampleFacility/301:14-3014/device/3014")
@@ -174,17 +174,17 @@ class TestBAS(unittest.TestCase):
 
   def test_convert_bacnet_data_to_rdf_no_data(self):
     bacnet_data = bytes(json.dumps([]), 'utf-8')
-    g = self.bas.convert_bacnet_data_to_rdf(bacnet_data)
+    g = self.bacnet.convert_bacnet_data_to_rdf(bacnet_data)
     assert len(g) == 0
   
   def test_convert_bacnet_data_to_rdf_invalid_data(self):
     bacnet_data = bytes(json.dumps([{"invalid": "data"}]), 'utf-8')
     with self.assertRaises(Exception):
-      self.bas.convert_bacnet_data_to_rdf(bacnet_data)
+      self.bacnet.convert_bacnet_data_to_rdf(bacnet_data)
     
   def test_convert_bacnet_data_to_rdf_missing_keys(self):
     bacnet_data = bytes(json.dumps([{"Bacnet Data": "{}"}]), 'utf-8')
-    g = self.bas.convert_bacnet_data_to_rdf(bacnet_data)
+    g = self.bacnet.convert_bacnet_data_to_rdf(bacnet_data)
     assert len(g) == 0
   
   def test_upload_bacnet_data(self):
@@ -201,11 +201,11 @@ class TestBAS(unittest.TestCase):
       }
     ]
     session_mock.run.return_value = mock_query_result
-    self.bas.upload_bacnet_data(bacnet_data)
-    self.bas.blob_store.upload_file.assert_called_once()
+    self.bacnet.upload_bacnet_data(bacnet_data)
+    self.bacnet.blob_store.upload_file.assert_called_once()
 
-  @patch('openoperator.core.bas.BAS.devices')
-  @patch('openoperator.core.bas.BAS.points')
+  @patch('openoperator.core.bacnet.BACnet.devices')
+  @patch('openoperator.core.bacnet.BACnet.points')
   def test_vectorize_graph(self, mock_points, mock_devices):
     mock_devices.return_value = [
       {
@@ -219,13 +219,13 @@ class TestBAS(unittest.TestCase):
         "uri": "https://openoperator.com/facility/point"
       }
     ]
-    self.bas.embeddings.create_embeddings.return_value = [Mock(embedding=[0.1, 0.2, 0.3])]
+    self.bacnet.embeddings.create_embeddings.return_value = [Mock(embedding=[0.1, 0.2, 0.3])]
     session_mock = self.setup_session_mock()
 
-    self.bas.vectorize_graph()
+    self.bacnet.vectorize_graph()
 
-    assert self.bas.embeddings.create_embeddings.call_count == 2
-    self.bas.embeddings.create_embeddings.assert_called_with(['test_point'])
+    assert self.bacnet.embeddings.create_embeddings.call_count == 2
+    self.bacnet.embeddings.create_embeddings.assert_called_with(['test_point'])
 
     # Check calls to session.run contain the correct Cypher query for devices and points
     device_query_call = [call for call in session_mock.run.call_args_list if "MATCH (n:Device)" in str(call)]
@@ -233,7 +233,7 @@ class TestBAS(unittest.TestCase):
     self.assertTrue(device_query_call, "Device vectors not uploaded correctly.")
     self.assertTrue(point_query_call, "Point vectors not uploaded correctly.")
 
-  @patch('openoperator.core.bas.BAS.devices')
+  @patch('openoperator.core.bacnet.BACnet.devices')
   def test_cluster_devices(self, mock_devices):
     mock_devices.return_value = [
       {
@@ -258,7 +258,7 @@ class TestBAS(unittest.TestCase):
       }
     ]
 
-    clusters = self.bas.cluster_devices()
+    clusters = self.bacnet.cluster_devices()
 
     assert len(clusters) == 2
     assert len(clusters[0]) == 3
